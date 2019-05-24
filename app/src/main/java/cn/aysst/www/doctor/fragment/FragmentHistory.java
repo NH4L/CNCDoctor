92f2acc4a2632b963b77f7d8c5db5a1962700a0d
NH4L <nh4l@qq.com>
2019-05-25 03:14:50 +0800

    the history fragment

--- app/src/main/java/cn/aysst/www/doctor/fragment/FragmentHistory.java
+++ app/src/main/java/cn/aysst/www/doctor/fragment/FragmentHistory.java
@@ -1,8 +1,14 @@
 package cn.aysst.www.doctor.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 import cn.aysst.www.doctor.Adapter.MyHistoryAdapter;
 import cn.aysst.www.doctor.R;
@@ -12,8 +18,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import cn.aysst.www.doctor.beans.CNCProblem;
+import cn.aysst.www.doctor.utils.Http;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.concurrent.TimeUnit;
 
 public class FragmentHistory extends Fragment {
@@ -30,22 +44,16 @@ public class FragmentHistory extends Fragment {
 		FragmentHistory fragment = new FragmentHistory();
 		return fragment;
 	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.activity_fragment_history, null);
+
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_cnc_history);
         adapter = new MyHistoryAdapter(getActivity());
+        initMyHistory();
         //设置LayoutManager为LinearLayoutManager
         layoutManager= new LinearLayoutManager(getActivity());
-        for(int i=0; i<5; i++){
-            CNCProblem CNCProblem =new CNCProblem();
-            CNCProblem.setBrand("fanuc");
-            CNCProblem.setQuestion("主轴温度也忒高了吧" + i);
-            CNCProblem.setQuestype("系统报警");
-            CNCProblem.setType("OA-i");
-            CNCProblem.setSolution("往机床里面浇水就行啦，直接大水漫灌!!");
-            adapter.addItem(CNCProblem);
-        }
 
         swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swip_refresh_onhistory);
         swipeRefreshLayout.setColorSchemeResources(R.color.btn);
@@ -63,23 +71,170 @@ public class FragmentHistory extends Fragment {
 		return view;
 	}
 
+
     private void refreshTask(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(2000);
+                //刷新时间1秒
+                try{
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                refreshMyHistory();
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(getActivity(), "正在刷新", Toast.LENGTH_SHORT);
+                        Toast.makeText(getActivity(),"刷新成功", Toast.LENGTH_SHORT).show();
+                        swipeRefreshLayout.setRefreshing(false);
+
                     }
                 });
             }
         }).start();
     }
 
+    private void initMyHistory() {
+        new InitMyHistoryAsyncTask().execute();
+    }
+
+    class InitMyHistoryAsyncTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            return httpPostGetHistoryReq();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    int code = obj.getInt("code");
+                    String msg = obj.getString("msg");
+                    Log.d(Http.TAG, "code=" + code + ", msg=" + msg);
+                    JSONObject data = obj.getJSONObject("data");
+                    if (code == 0 && msg.equals("success")) {
+                        int historyCount = data.getInt("count");
+                        for (int i=historyCount-1; i>=0; i--) {
+                            JSONObject object = data.getJSONObject("problem" + i);
+                            String brand = object.getString("brand");
+                            String question = object.getString("question");
+                            String questype = object.getString("questype");
+                            String solution = object.getString("solution");
+                            String type = object.getString("type");
+
+                            question = Http.cutStringTitle(question);
+                            solution = Http.cutStringContent(solution);
+                            brand = Http.cutStringSmall(brand);
+
+                            CNCProblem cncProblem = new CNCProblem();
+                            cncProblem.setBrand(brand);
+                            cncProblem.setQuestion(question);
+                            cncProblem.setQuestype(questype);
+                            cncProblem.setType(type);
+                            cncProblem.setSolution(solution);
+                            adapter.addItem(cncProblem);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void refreshMyHistory() {
+        new refreshMyHistoryAsyncTask().execute();
+    }
+
+    class refreshMyHistoryAsyncTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            return httpPostGetHistoryReq();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    int code = obj.getInt("code");
+                    String msg = obj.getString("msg");
+                    Log.d(Http.TAG, "code=" + code + ", msg=" + msg);
+                    JSONObject data = obj.getJSONObject("data");
+                    if (code == 0 && msg.equals("success")) {
+                        int historyCount = data.getInt("count");
+                        for (int i=historyCount-1; i>=0; i--) {
+                            JSONObject object = data.getJSONObject("problem" + i);
+                            String brand = object.getString("brand");
+                            String question = object.getString("question");
+                            String questype = object.getString("questype");
+                            String solution = object.getString("solution");
+                            String type = object.getString("type");
+
+                            question = Http.cutStringTitle(question);
+                            solution = Http.cutStringContent(solution);
+                            brand = Http.cutStringSmall(brand);
+
+                            CNCProblem cncProblem = new CNCProblem();
+                            cncProblem.setBrand(brand);
+                            cncProblem.setQuestion(question);
+                            cncProblem.setQuestype(questype);
+                            cncProblem.setType(type);
+                            cncProblem.setSolution(solution);
+
+
+                            adapter.addFirstItem(cncProblem);
+
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void parseHistoryJson(String result) {
+
+    }
+
+    private String httpPostGetHistoryReq() {
+	    String result = null;
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String name = preferences.getString("name", "");
+        FormBody body = new FormBody.Builder()
+                .add("username", name)
+                .build();
+
+        final Request request = new Request.Builder()
+                .url(Http.BASE_URL + "/cnc/gethistory")
+                .post(body)
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                result = response.body().string();
+                Log.d("myList", result);
+                result = URLDecoder.decode(result, "UTF-8");
+            } else {
+                throw new IOException("Unexpected code:" + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
